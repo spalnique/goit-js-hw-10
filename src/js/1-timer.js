28aa8d3e70a42bca45c65f0973d18db1ca7d104b
Oleksii_S <lex.sydoruk@gmail.com>
2024-01-28 20:27:27 +0100

    .

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -3,6 +3,16 @@ import 'flatpickr/dist/flatpickr.min.css';
 import izitoast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import errorIcon from '../img/izitoast-icon.svg';
+import closeIcon from '../img/izitoast-close.svg';
+
+const userLinks = {
+  startButton: document.querySelector('[data-start]'),
+  input: document.querySelector('#datetime-picker'),
+  daysElement: document.querySelector('[data-days]'),
+  hoursElement: document.querySelector('[data-hours]'),
+  minutesElement: document.querySelector('[data-minutes]'),
+  secondsElement: document.querySelector('[data-seconds]'),
+};
 
 class UserTimer {
   static #second = 1000;
@@ -11,7 +21,13 @@ class UserTimer {
   static #day = UserTimer.#hour * 24;
   #intervalID = null;
 
-  constructor(ms) {
+  constructor(ms, paramsObject) {
+    this.hoursElement = paramsObject.hoursElement;
+    this.minutesElement = paramsObject.minutesElement;
+    this.secondsElement = paramsObject.secondsElement;
+    this.startButton = paramsObject.startButton;
+    this.input = paramsObject.input;
+    this.daysElement = paramsObject.daysElement;
     this.days = ms.getDate() || 0;
     this.hours = ms.getHours() || 0;
     this.minutes = ms.getMinutes() || 0;
@@ -31,53 +47,50 @@ class UserTimer {
     return { days, hours, minutes, seconds };
   }
 
-  static validateDate(date) {
+  static isDateValid(date) {
     return date - new Date() > 0 ? true : false;
   }
 
-  startTimer(date, clearIntervalId) {
-    if (UserTimer.validateDate(date)) {
-      clearInterval(clearIntervalId);
-      document.querySelector('[data-start]').classList.remove('js-enabled');
-      document.querySelector('#datetime-picker').classList.add('js-disabled');
-      document.querySelector('#datetime-picker').disabled = true;
-      this.#intervalID = setInterval(
-        () => {
-          const timeDiff = date - new Date();
-          const timeRemaining = this.#convertMs(timeDiff);
-
-          document.querySelector('[data-days]').textContent = String(
-            timeRemaining.days
-          ).padStart(2, '0');
-          document.querySelector('[data-hours]').textContent = String(
-            timeRemaining.hours
-          ).padStart(2, '0');
-          document.querySelector('[data-minutes]').textContent = String(
-            timeRemaining.minutes
-          ).padStart(2, '0');
-          document.querySelector('[data-seconds]').textContent = String(
-            timeRemaining.seconds
-          ).padStart(2, '0');
-          if (!UserTimer.validateDate(date)) {
-            document.querySelector('#datetime-picker').disabled = false;
-            document
-              .querySelector('#datetime-picker')
-              .classList.remove('js-disabled');
-            this.#stopTimer(this.#intervalID);
-          }
-        },
-        1000,
-        date
-      );
+  startTimer(date) {
+    if (UserTimer.isDateValid(date)) {
+      this.startButton.classList.remove('js-enabled');
+      this.input.classList.add('js-disabled');
+      this.input.disabled = true;
+      this.#intervalID = setInterval(() => {
+        const timeDiff = date - new Date();
+        const timeRemaining = this.#convertMs(timeDiff);
+        console.log('tick');
+        this.daysElement.textContent = String(timeRemaining.days).padStart(
+          2,
+          '0'
+        );
+        this.hoursElement.textContent = String(timeRemaining.hours).padStart(
+          2,
+          '0'
+        );
+        this.minutesElement.textContent = String(
+          timeRemaining.minutes
+        ).padStart(2, '0');
+        this.secondsElement.textContent = String(
+          timeRemaining.seconds
+        ).padStart(2, '0');
+        if (!UserTimer.isDateValid(date)) {
+          this.input.disabled = false;
+          document
+            .querySelector('#datetime-picker')
+            .classList.remove('js-disabled');
+          this.#stopTimer();
+        }
+      }, 1000);
     }
   }
 
-  #stopTimer(clearIntervalId) {
-    clearInterval(clearIntervalId);
-    document.querySelector('[data-days]').textContent = '00';
-    document.querySelector('[data-hours]').textContent = '00';
-    document.querySelector('[data-minutes]').textContent = '00';
-    document.querySelector('[data-seconds]').textContent = '00';
+  #stopTimer() {
+    clearInterval(this.#intervalID);
+    this.daysElement.textContent = '00';
+    this.hoursElement.textContent = '00';
+    this.minutesElement.textContent = '00';
+    this.secondsElement.textContent = '00';
   }
 }
 
@@ -86,43 +99,54 @@ flatpickr('#datetime-picker', {
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-    const timer = new UserTimer(selectedDates[0]);
-    if (!UserTimer.validateDate(selectedDates[0])) {
-      izitoast.show({
-        class: 'js-izitoast-message',
-        message: 'Please choose a date in the future',
-        messageColor: '#FFFFFF',
-        messageSize: '16px',
-        position: 'topRight',
-        backgroundColor: '#EF4040',
-        progressBarColor: '#B51B1B',
-        iconUrl: errorIcon,
-        displayMode: 'replace',
-        closeOnEscape: true,
-        pauseOnHover: false,
-      });
-      return;
-    }
-    const isValidDate = setInterval(() => {
-      if (!UserTimer.validateDate(selectedDates[0])) {
-        document.querySelector('[data-start]').classList.remove('js-enabled');
-        document
-          .querySelector('[data-start]')
-          .addEventListener(
-            'click',
-            () => izitoast.error({ displayMode: 'once' }),
-            {
-              once: true,
-            }
-          );
-      } else {
-        document.querySelector('[data-start]').classList.add('js-enabled');
-        document.querySelector('[data-start]').addEventListener('click', () => {
-          timer.startTimer(selectedDates[0], isValidDate);
-        });
-      }
-    }, 1000);
-  },
+  onClose: onClose,
 });
+
+function onClose(selectedDates) {
+  console.log(selectedDates[0]);
+  const closeTimestamp = Date.now();
+  const timer = new UserTimer(selectedDates[0], userLinks);
+  if (!UserTimer.isDateValid(selectedDates[0])) {
+    showMessage('Please choose a date in the future');
+    return;
+  }
+  const timeoutID = setTimeout(() => {
+    timer.startButton.classList.remove('js-enabled');
+    timer.startButton.disabled = true;
+    showMessage('Selected date is not valid anymore');
+  }, selectedDates[0] - closeTimestamp);
+
+  if (UserTimer.isDateValid(selectedDates[0])) {
+    timer.startButton.classList.add('js-enabled');
+    timer.startButton.disabled = false;
+    timer.startButton.addEventListener('click', () => {
+      clearTimeout(timeoutID);
+      timer.startTimer(selectedDates[0]);
+    });
+  }
+}
+
+function showMessage(messageText) {
+  izitoast.show({
+    class: 'js-izitoast-message',
+    message: messageText,
+    messageColor: '#FFFFFF',
+    messageSize: '16px',
+    position: 'topRight',
+    backgroundColor: '#EF4040',
+    progressBarColor: '#B51B1B',
+    iconUrl: errorIcon,
+    displayMode: 'replace',
+    close: false,
+    closeOnEscape: true,
+    pauseOnHover: false,
+    buttons: [
+      [
+        `<button type="button" style="background-color: transparent;"><img src=${closeIcon}></button>`,
+        function (instance, toast) {
+          instance.hide({ transitionOut: 'fadeOut' }, toast);
+        },
+      ],
+    ],
+  });
+}
